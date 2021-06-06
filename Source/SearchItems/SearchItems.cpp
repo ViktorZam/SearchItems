c28ae9c11d3d09dc9d0c6c20f719c2dc8178b65e
ViktorZ <zamiralov3ddd@mail.ru>
2021-06-06 19:20:03 +0300

    add assets

---
+++ Source/SearchItems/SearchItems.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SearchItems.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SearchItems, "SearchItems" );
